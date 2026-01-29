fbdd0f21906621c27c509d13927c207af2cb3966
zhangxinxin <1285367184@qq.com>
2026-01-29 23:30:21 +0800

    02_5种http数据传输方式-form data.

--- 02_5种http数据传输方式/src/person/person.controller.ts
+++ 02_5种http数据传输方式/src/person/person.controller.ts
@@ -7,10 +7,13 @@ import {
   Param,
   Delete,
   Query,
+  UseInterceptors,
+  UploadedFiles,
 } from '@nestjs/common';
 import { PersonService } from './person.service';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('person')
 export class PersonController {
@@ -34,5 +37,23 @@ export class PersonController {
   @Post('create')
   testJson(@Body() createpersonDto: CreatePersonDto) {
     return `createPersonDto:${JSON.stringify(createpersonDto)}`;
+  }
+
+  @Post('file')
+
+  // 使用UseInterceptors装饰器启用AnyFilesInterceptor拦截器
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      dest: 'uploads/', // 指定上传的文件保存在哪个目录,在此指定目录名字,没有则会自动新建目录.
+    }),
+  )
+
+  // 通过UploadedFiles装饰器来取出文件参数, 普通的参数还是通过Body装饰器取出.
+  testFormData(
+    @Body() createPersonDto: CreatePersonDto,
+    @UploadedFiles() files: Array<Express.Multer.File>,
+  ) {
+    console.log(files);
+    return `createPersonDto: ${JSON.stringify(createPersonDto)}`;
   }
 }
